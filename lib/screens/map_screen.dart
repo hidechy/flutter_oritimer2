a3c2f5fde66768be09f0ba8b0bb29b2201878f00
hidechy <hide.toyoda@gmail.com>
2023-11-08 16:30:48 +0900

    commit 20231108

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,11 +1,9 @@
 // ignore_for_file: must_be_immutable, depend_on_referenced_packages, deprecated_member_use
 
 import 'dart:async';
-import 'dart:math';
 
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -32,6 +30,8 @@ class MapScreen extends ConsumerWidget {
   Completer<GoogleMapController> mapController = Completer<GoogleMapController>();
 
   LatLngBounds pinpointMapBounds = LatLngBounds(southwest: const LatLng(0, 0), northeast: const LatLng(0, 0));
+
+  late Set<Marker> markers;
 
   late BuildContext _context;
   late WidgetRef _ref;
@@ -64,11 +64,32 @@ class MapScreen extends ConsumerWidget {
 
     setMapParam();
 
-    makeBounds();
-
     final now = DateTime.now();
     final timeFormat = DateFormat('HH:mm:ss');
     final currentTime = timeFormat.format(now);
+
+    //==============================
+    markers = {
+      Marker(
+        markerId: const MarkerId('here'),
+        position: LatLng(latLngState.lat, latLngState.lng),
+        infoWindow: const InfoWindow(title: 'title', snippet: 'snippet'),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRose),
+      ),
+    };
+
+    if (selectedTrainStation != null) {
+      markers.add(
+        Marker(
+          markerId: const MarkerId('station'),
+          position: LatLng(selectedTrainStation!.lat.toDouble(), selectedTrainStation!.lng.toDouble()),
+          infoWindow: const InfoWindow(title: 'title', snippet: 'snippet'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRose),
+        ),
+      );
+    }
+
+    //==============================
 
     return Scaffold(
       body: SafeArea(
@@ -76,10 +97,9 @@ class MapScreen extends ConsumerWidget {
           children: [
             //=======================================
             GoogleMap(
-              onMapCreated: _onMapCreated,
+              onMapCreated: mapController.complete,
               initialCameraPosition: initialCameraPosition,
-              // markers: markers,
-              // polylines: polylineSet,
+              markers: markers,
               zoomControlsEnabled: false,
             ),
             //=======================================
@@ -140,46 +160,6 @@ class MapScreen extends ConsumerWidget {
   }
 
   ///
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = Completer();
-
-    makeBounds();
-  }
-
-  ///
-  Future<void> makeBounds() async {
-    final latList = <double>[];
-    final lngList = <double>[];
-
-    final latLngState = _ref.watch(latLngProvider);
-
-    latList.add(latLngState.lat);
-    lngList.add(latLngState.lng);
-
-    selectedTrainStation = await _ref.watch(trainStationProvider.select((value) => value.selectedTrainStation));
-
-    if (selectedTrainStation != null) {
-      latList.add(selectedTrainStation!.lat.toDouble());
-      lngList.add(selectedTrainStation!.lng.toDouble());
-    }
-
-    final minLat = latList.reduce(min);
-    final maxLat = latList.reduce(max);
-    final minLng = lngList.reduce(min);
-    final maxLng = lngList.reduce(max);
-
-    pinpointMapBounds = LatLngBounds(northeast: LatLng(maxLat, maxLng), southwest: LatLng(minLat, minLng));
-
-    await _displayPinpointMapBounds();
-  }
-
-  ///
-  Future<void> _displayPinpointMapBounds() async {
-    final googleMap = await mapController.future;
-    await googleMap.animateCamera(CameraUpdate.newLatLngBounds(pinpointMapBounds, 50));
-  }
-
-  ///
   Future<void> getLocation() async {
     var permission = await Geolocator.checkPermission();
 
@@ -193,78 +173,13 @@ class MapScreen extends ConsumerWidget {
 
     final position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 
-    // debugPrint('緯度: ${position.latitude}');
-    // debugPrint('経度: ${position.longitude}');
-
     final param = LatLngRequestState(lat: position.latitude, lng: position.longitude);
 
     await _ref.read(latLngProvider.notifier).setLatLng(param: param);
-
-/*
-    //
-
-    // 高度
-    debugPrint('高度: ${position.altitude}');
-
-    //
-
-    final exFunabashi = funabashi.split(',');
-    final exZenpukuji = zenpukuji.split(',');
-
-    // 距離をメートルで返す
-    final distanceInMeters = Geolocator.distanceBetween(
-      exFunabashi[0].toDouble(),
-      exFunabashi[1].toDouble(),
-      exZenpukuji[0].toDouble(),
-      exZenpukuji[1].toDouble(),
-    );
-
-    debugPrint('距離:$distanceInMeters');
-
-    // 方位を返す
-    final bearing = Geolocator.bearingBetween(
-      exFunabashi[0].toDouble(),
-      exFunabashi[1].toDouble(),
-      exZenpukuji[0].toDouble(),
-      exZenpukuji[1].toDouble(),
-    );
-
-    debugPrint('方位:$bearing');
-    */
   }
-
-  // ///
-  // Future<void> _makeMarker() async {
-  //   markerList = [];
-  //
-  //   final latLngState = _ref.watch(latLngProvider);
-  //
-  //   markerList.add(
-  //     Marker(
-  //       point: LatLng(latLngState.lat, latLngState.lng),
-  //       child: CircleAvatar(
-  //         backgroundColor: Colors.orangeAccent.withOpacity(0.4),
-  //         child: const Text(
-  //           'Here',
-  //           style: TextStyle(color: Colors.white, fontSize: 10, fontWeight: FontWeight.bold),
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-  //
-  //
 
   ///
   void setMapParam() {
-    // final pinpointMapZoom = _ref.watch(mapPinpointProvider.select((value) => value.pinpointMapZoom));
-    //
-    // final latLng = LatLng(geolocList[0].latitude.toDouble(), geolocList[0].longitude.toDouble());
-    // initialCameraPosition = CameraPosition(target: latLng, zoom: pinpointMapZoom.toDouble(), tilt: 50);
-    //
-    //
-    //
-
     final latLngState = _ref.watch(latLngProvider);
     final latLng = LatLng(latLngState.lat, latLngState.lng);
     initialCameraPosition = CameraPosition(target: latLng, zoom: 15, tilt: 50);
